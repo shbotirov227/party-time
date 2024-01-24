d05dc20baadc910c60ebc7a229997ed144f5a911
Shohruh Botirov <shbotirov227@gmail.com>
2024-01-24 19:31:14 +0500

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,12 +12,12 @@ const About = () => {
 					<h3 className="About-title">Welcome to Party time - Eventick</h3>
 
 					<p className="About-text">
-						Welcome to Party Time Rental!
+						Welcome to Party time - Eventick !
 
 
 						Thank you for taking the time to visit our website and considering us to be your rental provider! We would love to partner with you for your upcoming event. Pole Tent
 
-						Party Time Rental humbly began in 1983. Harmoniously with our genuine passion for the industry, it has grown to be one of the largest rental companies serving the Rocky Mountain Region. With convenient  locations in both Denver and Colorado Springs, we hold the largest inventory of rentals in Colorado. So we can gladly supply to all events.
+						Party time - Eventick humbly began in 1983. Harmoniously with our genuine passion for the industry, it has grown to be one of the largest rental companies serving the Rocky Mountain Region. With convenient  locations in both Denver and Colorado Springs, we hold the largest inventory of rentals in Colorado. So we can gladly supply to all events.
 
 						While our passion, experience, knowledge, inventory, fleet, and staff have grown over the years, our belief that all events, no matter the size, are special and important, has remained true. Whether you are planning an intimate dinner for two or a large outdoor festival for over 10,000, you will Party Time Truckreceive one on one attention from our Tent & Event Specialists who are eager to learn more about your event and help supply you with the best and right equipment.
 
